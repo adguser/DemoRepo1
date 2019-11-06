2eb3984240a4d35bedb5b68ae45354f4fd93d8b4
adguser <adguserone@gmail.com>
2019-11-05 20:37:59 -0500

    demo project

---
+++ src/main/java/com/qa/Base/Base.java
@@ -0,0 +1,13 @@
+package com.qa.Base;
+
+/**
+ * Hello world!
+ *
+ */
+public class Base 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
